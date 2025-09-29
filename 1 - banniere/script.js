1e0979fc4de63a847edd857b84f6cb43f49b8d0b
chrisbautista09 <chrisbautista@hotmail.fr>
2025-09-29 09:51:07 +0200

    la bannière

--- 1 - banniere/script.js
+++ 1 - banniere/script.js
@@ -6,6 +6,16 @@ Nouvelles compétences:
 
 // 1. Je recupere le bouton "Accepter les cookies"
 
+let btncookies_elem = document.querySelector(".btn.btn-accept");
+
 // 2. Je recupere la banniere de cookies
 
+const cookies_elem = document.querySelector(".cookies");
+
 // 3. J'ecoute l'evenement "click" sur le bouton "Accepter les cookies"
+
+
+cookies_elem.addEventListener("click",() => {
+      cookies_elem .classList.add("hidden"); 
+    });
+
--- 2 - abonne/script.js
+++ 2 - abonne/script.js
@@ -12,10 +12,16 @@ Nouvelles compétences:
 
 // 1. Je recupere l'icone smiley
 
+const smiley_elem =  document.querySelector(".fa-solid fa-face-meh");
+
 // 2. Je recupere le bouton ABONNER
+
+const btnSub_elem =  document.querySelector(".btn-sub");
+
 
 // 3. Je défini une variable binaire qui exprime l'etat abonné ou non de l'utilisateur
 
+let subscriberState_elem = 
 // 4. J'ecoute le clique sur l'icone smiley
 
 // 5. J'ecoute l'evenemment click sur le bouton ABONNEZ
