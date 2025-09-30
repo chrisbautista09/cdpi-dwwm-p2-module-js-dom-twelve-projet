3b847f29f0cc095a5158f88d496c9035e9a0a0a5
chrisbautista09 <chrisbautista@hotmail.fr>
2025-09-30 16:17:09 +0200

    MenuBurger

--- 4 - menuburger/script.js
+++ 4 - menuburger/script.js
@@ -1,5 +1,16 @@
 
-// 1. Je recupere le burger
+// 1. Je récupère le bouton burger dans le DOM
 const burger = document.querySelector(".burger");
-// 2. Je recupere la fenetre modal
+
+// 2. Je récupère la fenêtre (ou le menu) modal dans le DOM
 const modal = document.querySelector(".modal");
+
+// 3. J’écoute le clic sur le bouton burger
+burger.addEventListener("click", function () {
+
+    // 3.1 J’ajoute/retire la classe "fa-x" au burger 
+        burger.classList.toggle("fa-x");
+
+    // 3.2 J’ajoute/retire la classe "show-modal" à la fenêtre modal
+        modal.classList.toggle("show-modal");
+});
