ca826a2d5dd809420e514c98b8a27fcfbd60156e
chrisbautista09 <chrisbautista@hotmail.fr>
2025-10-01 14:29:57 +0200

    Toast

--- 6 - toast/script.js
+++ 6 - toast/script.js
@@ -11,3 +11,27 @@
 const btn = document.querySelector("button");
 // 2. Je recupere le conteneur de toasts
 const ctnToasts = document.querySelector(".container-toasts");
+
+// création des  balises 
+const toast = document.createElement("div"); 
+const toastp = document.createElement("p");
+
+// Personnalisation de la balise
+toast.classList.add("toast");
+toast.classList.add("text_content");
+toast.innerText = "Le fichier a bien été enregistré";
+
+// Affichage de la nouvelle balise en tant qu'enfant de la div.
+btn.addEventListener("click",function(){
+
+toast.appendChild(toastp);
+ctnToasts.appendChild(toast);
+});
+
+// Suppression d'une balise
+ setTimeout(() => {
+        toast.remove();
+    }, 3000);
+
+
+
