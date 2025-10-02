db455b5e3a3202eb27d955e2aee590dbd14f8c32
chrisbautista09 <chrisbautista@hotmail.fr>
2025-10-02 11:52:58 +0200

    Fiche Produit

--- 8 - Fiche produit/script.js
+++ 8 - Fiche produit/script.js
@@ -19,4 +19,22 @@ const panierMsg = document.querySelector(".panier-msg");
 // 5. Je crée une variable qui contient le nombre de produits ajoutées ua panier
 let panier = 0;
 
-// ...
+// 6. J’ajoute un écouteur de clic sur chaque vignette
+vignettes.forEach(function(vignette) {
+
+    vignette.addEventListener("click", function() {
+
+        // Je récupère la source de la vignette cliquée
+        const newUrl = vignette.getAttribute("src");
+
+        // Je mets cette source dans la grande image
+        fullImg.setAttribute("src", newUrl);
+    });
+});
+
+// 7. J'incrémente mon panier de +1 à chaque click sur le btn "ajouter au panier"
+btnAdd.addEventListener("click",function() {
+ panier ++;
+panierMsg.innerText = `Vous avez ${panier}  produit(s) dans votre panier.`;
+});
+
