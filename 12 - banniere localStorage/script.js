735b4bbd30f79b2b901425e2962dc5cd16fe9b1c
chrisbautista09 <chrisbautista@hotmail.fr>
2025-10-03 10:00:20 +0200

    Banniére Localstorage

--- 12 - banniere localStorage/script.js
+++ 12 - banniere localStorage/script.js
@@ -4,3 +4,22 @@
  * getItem()
  * Fonctionnement de Stockage et des cookies
  */
+/*
+Nouvelles compétences:
+- querySelector // Selectionne le premier element seulement mieux que getElementsBy
+- HTMLElement.style 
+*/
+
+// 1. Je recupere le bouton "Accepter les cookies"
+const acceptBtn = document.querySelector(".btn-accept");
+// 2. Je recupere la banniere de cookies
+const cookiesBanner = document.querySelector(".cookies");
+
+// 3. J'ecoute l'evenement "click" sur le bouton "Accepter les cookies"
+acceptBtn.addEventListener("click",function(e){
+    // 4. Je rend transparent la bannière pour effectuer une jolie animation de "fadeOut", chose impossible avec la proprieté "display:none"
+    cookiesBanner.style.opacity = 0;
+    // 5. Set Item
+    localStorage.setItem("cookiesBanner", "accept");
+    console.log(localStorage);
+});
